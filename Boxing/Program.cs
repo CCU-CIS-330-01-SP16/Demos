44b2e5f9e7f310ce5154dded7dfe0babadb2ec3f
Matt Anderson <mattanderson@ga.ccu.edu>
2016-01-04 21:45:47 -0700

    Added Boxing solution.

---
+++ Boxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            for(int i = 0; i < 10000000; i++)
+            {
+                object obj = i;
+                //int y = i;
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        }
+    }
+}
