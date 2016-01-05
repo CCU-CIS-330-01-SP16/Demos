cacea58c1b378744bcdfb24bf300a0ef69fc33b9
Matt Anderson <mattanderson@ga.ccu.edu>
2016-01-04 21:16:11 -0700

    Added ParameterKeywords solution.

---
+++ ParameterKeywords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 2;
+            int result;
+
+            Console.WriteLine(value);
+            Square(value, out result);
+            Console.WriteLine(result);
+        }
+
+        static void Square(ref int x)
+        {
+            x *= x;
+        }
+
+        static void Square(int x, out int y)
+        {
+            y = x * x;
+        }
+    }
+}
