8f1b086ab38e6798a5234fb2626e1c8bc9fa02a6
Matt Anderson <mattanderson@ga.ccu.edu>
2016-02-06 13:32:59 -0700

    Added Lecture9Demos solution

---
+++ Lecture9Demos/Lecture9Demos/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9Demos
+{
+    class Animal
+    {
+        public int NumberOfLegs { get; set; }
+    }
+}
---
+++ Lecture9Demos/Lecture9Demos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            //Console.WriteLine("FullName: {0}", currentAssembly.FullName);
+            //Console.WriteLine("Location: {0}", currentAssembly.Location);
+            //Console.WriteLine("CodeBase: {0}", currentAssembly.CodeBase);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Modules:");
+            //foreach (Module module in currentAssembly.Modules)
+            //{
+            //    Console.WriteLine("Module FQN: {0}", module.FullyQualifiedName);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("Attributes:");
+            //foreach (var attribute in currentAssembly.CustomAttributes)
+            //{
+            //    Console.WriteLine(attribute);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("Types:");
+            //foreach (var type in currentAssembly.GetTypes())
+            //{
+            //    Console.WriteLine(type);
+            //}
+        }
+    }
+}
