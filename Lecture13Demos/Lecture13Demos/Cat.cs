28d1277f763b312b4a427567d4148841fe1bce1a
Matt Anderson <mattanderson@ga.ccu.edu>
2016-02-21 13:04:29 -0700

    Added Lecture13Demos solution.

---
+++ Lecture13Demos/Lecture13Demos/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture13Demos
+{
+    // Required for Binary serialization.
+    [Serializable]
+    // Required for DataContract serialization.
+    [DataContract]
+    public class Cat : Animal
+    {
+        public Cat(string name = null)
+            : base(name)
+        {
+        }
+    }
+}
