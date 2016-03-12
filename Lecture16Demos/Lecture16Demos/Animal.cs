1f1471caed95f55767d71cf86e6202dafaf876ba
Matt Anderson <mattanderson@ga.ccu.edu>
2016-03-12 15:59:27 -0700

    Created Lecture16Demos solution.

---
+++ Lecture16Demos/Lecture16Demos/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture16Demos
+{
+    public abstract class Animal
+    {
+        public Animal()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public int NumberOfLegs { get; set; }
+
+        public virtual void Move() { }
+    }
+}
---
+++ Lecture16Demos/Lecture16Demos/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture16Demos
+{
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Ruff! Ruff!");
+        }
+
+        private void Breathe()
+        {
+            Console.WriteLine("Inhale...Exhale");
+        }
+    }
+}
