d9b0eea0cf4ffdc2722c1bdaf3deac38fb963497
Matt Anderson <mattanderson@ga.ccu.edu>
2016-04-02 11:10:16 -0600

    Added Lecture19Demos solution.

---
+++ Lecture19Demos/Lecture19Demos/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture19Demos
+{
+    class Logger : BaseLogger
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public new void LogCompleted()
+        {
+            Console.WriteLine("Finished");
+        }
+    }
+}
---
+++ Lecture19Demos/Lecture19Demos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lecture19Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Regex.Match("color", @"colou?r").Success);
+
+            Console.WriteLine(Regex.Match("say 25 miles more", @"\d+(?=\smiles)"));
+
+            Console.WriteLine(Regex.Match("say 25 miles more", @"(?<=say\s)\d+"));
+
+            ExamDemo();
+        }
+
+        private static void ExamDemo()
+        {
+            BaseLogger logger = new Logger();
+            logger.Log("Log started");
+            logger.Log("Base: Log continuing");
+            ((Logger)logger).LogCompleted();
+        }
+    }
+}
